d690cb1162ad8de163daeb6f8b545716d912cbf8
Brett <brett_neighbors@yahoo.com>
2024-03-02 20:46:33 -0700

    Created and routed list() in dishes. 6/68 pass.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,14 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+// Middleware Functions
+
+// CRUDL Functions
+function list(req, res) {
+    res.json({ data: dishes });
+}
+
+module.exports = {
+    list, 
+}
