76e284e072efe528f646b7134450bced0997d956
Brett <brett_neighbors@yahoo.com>
2024-03-02 21:53:12 -0700

    Created & exported list() in orders.controller 16/68 pass

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,15 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+// Middleware Functions
+
+// CRUDLE Functions
+
+function list(req, res) {
+    res.json({ data: orders });
+}
+
+module.exports = {
+    list,
+}
