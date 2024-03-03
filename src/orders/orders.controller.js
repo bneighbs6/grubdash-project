4b381066708f75457a2f17070a1ae2e568a82d07
Brett <brett_neighbors@yahoo.com>
2024-03-03 12:21:23 -0700

    Fixed error message for invaldDishes 28/68 pass

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -66,11 +66,15 @@ function dishesHasQuantity(req, res, next) {
     });
     // If invalidDishes array contains ANY length, return error status & message
     if (invalidDishes.length > 0) {
+        const errorMessage = invalidDishes.map((dish, index) => {
+          return `dish ${index} must have a quantity that is an integer greater than 0`;
+        }).join(", ");
+    
         return next({
-            status: 400,
-            message: `dish ${index} must have a quantity that is an integer greater than 0`
-        })
-    }
+          status: 400,
+          message: errorMessage,
+        });
+      }
     // IF invalidDishes array is empty, it moves on to next middleware fx
     return next(); 
 }
