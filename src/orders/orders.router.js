9cd7b95a89c0c1e4a578b4eb6f8cd8d6b7e6e256
Brett <brett_neighbors@yahoo.com>
2024-03-02 22:17:19 -0700

    Added methodNotAllowed to orders.router.js

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./orders.controller")
 // TODO: Implement the /orders routes needed to make the tests pass
 
 // New route for /
-router.route("/").get(controller.list).post(controller.create);
+router.route("/")
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed);
 
 module.exports = router;
