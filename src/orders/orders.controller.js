2f2ed8258ec0db14f19ec30ff7bb2838fdd42e33
Brett <brett_neighbors@yahoo.com>
2024-03-04 16:30:49 -0700

    Updated hasValidStatus to include proper checks

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -76,7 +76,7 @@ function dishesHasQuantity(req, res, next) {
 function hasValidStatus(req, res, next) {
     const { orderId } = req.params; 
     const foundOrder = orders.find((order) => order.id === orderId);
-    if (!foundOrder.status || foundOrder.status === "invalid") {
+    if (!foundOrder.status || foundOrder.status === "" || foundOrder.status === "invalid") {
         return next({
             status: 400,
             message: "Order must have a status of pending, preparing, out-for-delivery, or delivered."
