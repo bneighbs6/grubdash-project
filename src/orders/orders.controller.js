0f345462d1ee8a30855148cdea2d789511531deb
Brett <brett_neighbors@yahoo.com>
2024-03-04 10:11:26 -0700

    Fixed dishesHasQuantity w/ for loop 59/68 pass

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -49,27 +49,27 @@ function hasDishes(req, res, next) {
 function dishesHasQuantity(req, res, next) {
     const { data: { dishes } = {} } = req.body;
     
-    // Defines invalidDishes array
-    // Filters dishes that do not exist, or are less than 0
-    const invalidDishes = dishes.filter((dish, index) => {
-        if (!dish.hasOwnProperty('quantity') || !Number.isInteger(dish.quantity) || dish.quantity <= 0) {
-          return true;
+    for (let i = 0; i < dishes.length; i++) {
+        const dish = dishes[i];
+
+        // Checks if dish object does not have 'quantity' property
+        if (!dish.hasOwnProperty("quantity")) {
+            return next({
+                status: 400,
+                message: `dish ${i} must have a quantity that is an integer greater than 0`,
+            });
         }
-        return false;
-      });
-    // If invalidDishes array contains ANY length, return error status & message
-    if (invalidDishes.length > 0) {
-        const errorMessage = invalidDishes.map((dish, index) => {
-          return `dish ${index} must have a quantity that is an integer greater than 0`;
-        }).join(", ");
-    
-        return next({
-          status: 400,
-          message: errorMessage,
-        });
-      }
-    // IF invalidDishes array is empty, it moves on to next middleware fx
-    return next(); 
+
+        // Checks if 'quantity' property is not an inter or is less than 0
+        if (!Number.isInteger(dish.quantity) || dish.quantity < 0) {
+            return next({
+                status: 400,
+                message: `dish ${i} must have a quantity that is an integer greater than 0`,
+            });
+        }
+    }
+    // If no invalid dish found, move to next middleware
+    return next();
 }
 
 // Validates status is pending
